998d9faf16ce14ba8e10dc15310ce36d32733044
Gabriel Dickman <gabrieldickman33@gmail.com>
2023-07-13 20:46:26 -0400

    Criado o primeiro componente

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Navbar from '@/components/navbar/navbar'
 
 export default function Home() {
   return (
@@ -9,9 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        DevStart: A sua loja online de games
-      </div>
+      <nav>
+        <Navbar />
+      </nav>
     </>
   )
 }
