acdf9ed88de2840125280b16984fbf56c3a17489
Gabriel Dickman <gabrieldickman33@gmail.com>
2023-07-16 15:16:14 -0400

    Ajustado input da navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,15 @@
 import style from './navbar.module.css'
 import {BsCart4} from 'react-icons/bs'
 import Logo from '@/components/logo/logo'
+import Input from '@/components/forms/input/input'
 
 export default function Navbar(){
     return (
         <nav className={style.navbar}>
             <Logo />
-            <input />
+            <div className={style.search}>
+                <Input type="text" placeholder="Buscar jogos" fullWidth/>
+            </div>
             <BsCart4 size={45} />
         </nav>
     )
