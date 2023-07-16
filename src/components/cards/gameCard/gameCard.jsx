db66ee76075fe7a9ba727e4ffcf6d272be16568c
Gabriel Dickman <gabrieldickman33@gmail.com>
2023-07-16 15:32:46 -0400

    Corrigido responsividade

--- src/components/cards/gameCard/gameCard.jsx
+++ src/components/cards/gameCard/gameCard.jsx
@@ -5,7 +5,7 @@ import Button from '@/components/forms/button/button'
 export default function GameCard(){
     return(
         <div className={style.gamecard}>
-            <Image src='/products/counter-strike.jpg' width={300} height={145} alt='Counter Strike' />
+            <Image className={style.image} src='/products/counter-strike.jpg' width={300} height={145} alt='Counter Strike' />
             <div className={style.info}>
                 <h3 className={style.title}>Counter Strike: Global Offensive</h3>
                 <p className={style.category}>Ação, Estratégia, Multijogador</p>
